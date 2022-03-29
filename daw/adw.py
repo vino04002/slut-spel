c10709c4bbe2d3967490e3ab736d5ffd7b383d17
vino04002 <92526491+vino04002@users.noreply.github.com>
2022-03-29 13:49:26 +0200

    Add files via upload

---
+++ daw/adw.py
@@ -0,0 +1,18 @@
+import pygame
+from pygame.locals import *
+pygame.init()
+pygame.font.init()
+width, height = 64*10, 64*8
+screen=pygame.display.set_mode((width, height))
+player_x = 200
+player_y = 200
+player = pygame.image.load("hero.png")
+while 1:
+    screen.fill((255,255,255))
+    screen.blit(player, (player_x, player_y))
+    pygame.display.flip() 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+         
+            pygame.quit() 
+            exit(0)
